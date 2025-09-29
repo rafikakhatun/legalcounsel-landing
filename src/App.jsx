2601a7b367b0afc889841b180bf0fd802a6e28f5
rafika khatun <rafikakhatun607@gmail.com>
2025-09-29 19:55:51 +0530

    feat: install and configure AOS for scroll animations

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function App() {
   const [count, setCount] = useState(0)
+
+  AOS.init({
+  duration: 1000, 
+  once: true,     
+});
 
   return (
     <>
